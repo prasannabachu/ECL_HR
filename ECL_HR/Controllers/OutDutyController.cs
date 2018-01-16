2f2840a6d61356530dca4aae2197fbdfcff77844
Nagesh <nagesh@mdoffice.in>
2018-01-16 13:45:23 +0530

    Adding OutDuty Controller

---
+++ ECL_HR/Controllers/OutDutyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECL_HR.Controllers
+{
+    public class OutDutyController : Controller
+    {
+        // GET: OutDuty
+        public ActionResult outduty()
+        {
+            return View();
+        }
+    }
+}
