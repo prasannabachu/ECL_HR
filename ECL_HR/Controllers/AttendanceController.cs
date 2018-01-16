210f4504f1be8267039215a6b3ac69b266e5b4b4
ramesh <ramesh.b@mdoffice.in>
2018-01-16 13:33:54 +0530

    "first commit"

---
+++ ECL_HR/Controllers/AttendanceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECL_HR.Controllers
+{
+    public class AttendanceController : Controller
+    {
+        // GET: Attendance
+        public ActionResult Attendance()
+        {
+            return View();
+        }
+    }
+}
---
+++ ECL_HR/scripts/NewFolder1/UserDetails.js
@@ -0,0 +1,20 @@
+﻿angular.module('myApp').controller("UserControll", ['$scope', '$http', '$rootScope', function ($scope, $http, $rootScope) {
+      $scope.initMainView = function () {
+         this.GetUserDetails();
+     
+    };
+
+    
+    $scope.GetUserDetails = function () {
+        
+        $http({
+                   method: 'GET',
+                    url: '/Home/getEmployeeData'
+                }).then(function (success){
+                    $scope.UsersCollection = success.data[0];
+                },function (error){
+
+                });
+           };
+    
+}]);
