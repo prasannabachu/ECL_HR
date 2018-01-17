ff3fd52de825b602fe022e991420899990d0812d
mdoffice <bachuvlnk.prasanna@mdoffice.in>
2018-01-17 11:21:54 +0500

    "added new controllers and views"

---
+++ ECL_HR/Controllers/LanguageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECL_HR.Controllers
+{
+    public class LanguageController : Controller
+    {
+        // GET: Language sample
+        public ActionResult language()
+        {
+            return View();
+        }
+    }
+}
---
+++ ECL_HR/Controllers/PerformanceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECL_HR.Controllers
+{
+    public class PerformanceController : Controller
+    {
+        // GET: Performance
+        public ActionResult performance()
+        {
+            return View();
+        }
+    }
+}
